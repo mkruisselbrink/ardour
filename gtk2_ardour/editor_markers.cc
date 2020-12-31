e81baa764854e5dff6da260713794eb3a0f7710f
Paul Davis <paul@linuxaudiosystems.com>
2020-12-31 10:52:34 -0700

    fix next set of tempo map changing code blocks to use RCU

--- gtk2_ardour/editor_markers.cc
+++ gtk2_ardour/editor_markers.cc
@@ -1438,7 +1438,7 @@ Editor::toggle_tempo_type ()
 		Temporal::TempoPoint & tempo = tm->tempo();
 
 		begin_reversible_command (_("set tempo to constant"));
-		TempoMap::SharedPtr tmap (TempoMap::use());
+		TempoMap::SharedPtr tmap (TempoMap::write_copy());
 		XMLNode &before = tmap->get_state();
 
 		tmap->set_ramped (tempo, !tempo.ramped());
@@ -1446,6 +1446,8 @@ Editor::toggle_tempo_type ()
 		XMLNode &after = tmap->get_state();
 		_session->add_command (new MementoCommand<Temporal::TempoMap> (new Temporal::TempoMap::MementoBinder(), &before, &after));
 		commit_reversible_command ();
+
+		TempoMap::update (tmap);
 	}
 }
 /* clamped locks the previous section end tempo to the start tempo */
@@ -1459,7 +1461,7 @@ Editor::toggle_tempo_clamped ()
 	if (tm) {
 		begin_reversible_command (_("Clamp Tempo"));
 
-		TempoMap::SharedPtr tmap (TempoMap::use());
+		TempoMap::SharedPtr tmap (TempoMap::write_copy());
 		XMLNode &before = tmap->get_state();
 
 		Temporal::Tempo & tempo (tm->tempo());
@@ -1468,6 +1470,8 @@ Editor::toggle_tempo_clamped ()
 			XMLNode &after = tmap->get_state();
 			_session->add_command (new MementoCommand<Temporal::TempoMap> (new Temporal::TempoMap::MementoBinder(), &before, &after));
 			commit_reversible_command ();
+
+			TempoMap::update (tmap);
 		} else {
 			abort_reversible_command ();
 		}
@@ -1485,7 +1489,7 @@ Editor::ramp_to_next_tempo ()
 	if (tm) {
 
 		begin_reversible_command (_("ramp to next tempo"));
-		TempoMap::SharedPtr tmap (TempoMap::use());
+		TempoMap::SharedPtr tmap (TempoMap::write_copy());
 		XMLNode &before = tmap->get_state();
 
 		Temporal::TempoPoint & tempo (tm->tempo());
@@ -1493,6 +1497,8 @@ Editor::ramp_to_next_tempo ()
 			XMLNode &after = tmap->get_state();
 			_session->add_command (new MementoCommand<Temporal::TempoMap> (new Temporal::TempoMap::MementoBinder(), &before, &after));
 			commit_reversible_command ();
+
+			TempoMap::update (tmap);
 		} else {
 			abort_reversible_command ();
 		}
