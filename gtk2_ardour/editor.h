3e18d846f7cb1818b67206bbad1eac8d83c4cded
Paul Davis <paul@linuxaudiosystems.com>
2021-03-15 19:49:18 -0600

    make Editor::snap_to_bbt() public

--- gtk2_ardour/editor.h
+++ gtk2_ardour/editor.h
@@ -495,6 +495,9 @@ public:
 	                            GdkEvent const*      ev,
 	                            Temporal::RoundMode    direction = Temporal::RoundNearest,
 	                            ARDOUR::SnapPref     gpref = ARDOUR::SnapToAny_Visual);
+	Temporal::timepos_t snap_to_bbt (Temporal::timepos_t const & start,
+	                                 Temporal::RoundMode   direction,
+	                                 ARDOUR::SnapPref    gpref);
 
 	void set_snapped_cursor_position (Temporal::timepos_t const & pos);
 
@@ -2235,10 +2238,6 @@ private:
 	Temporal::timepos_t snap_to_cd_frames (Temporal::timepos_t const & start,
 	                                       Temporal::RoundMode   direction,
 	                                       ARDOUR::SnapPref    gpref);
-
-	Temporal::timepos_t snap_to_bbt (Temporal::timepos_t const & start,
-	                                 Temporal::RoundMode   direction,
-	                                 ARDOUR::SnapPref    gpref);
 
 	Temporal::timepos_t snap_to_timecode (Temporal::timepos_t const & start,
 	                                      Temporal::RoundMode   direction,
