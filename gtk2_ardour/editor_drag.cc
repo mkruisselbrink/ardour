c6966901bee1c01bdadc26c602d91fc784206c40
Paul Davis <paul@linuxaudiosystems.com>
2021-06-04 11:09:30 -0600

    add variables to determine scope of potential ripple at end of drag

--- gtk2_ardour/editor_drag.cc
+++ gtk2_ardour/editor_drag.cc
@@ -1622,6 +1622,10 @@ RegionMoveDrag::finished_copy (bool const changed_position, bool const /*changed
 	typedef map<boost::shared_ptr<Playlist>, RouteTimeAxisView*> PlaylistMapping;
 	PlaylistMapping playlist_mapping;
 
+	/* determine boundaries of dragged regions, across all playlists */
+	samplepos_t extent_min = max_samplepos;
+	samplepos_t extent_max = 0;
+
 	/* insert the regions into their (potentially) new (or existing) playlists */
 	for (list<DraggingView>::const_iterator i = _views.begin(); i != _views.end();) {
 
