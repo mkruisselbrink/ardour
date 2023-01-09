85764b6e6307d6815a373b18d8c60beda1de4a7c
Marijn Kruisselbrink <mek@google.com>
2023-01-08 16:19:57 -0800

    render more stuff better

--- gtk2_ardour/midi_score_bar.cc
+++ gtk2_ardour/midi_score_bar.cc
@@ -32,6 +32,8 @@ using Beats = Temporal::Beats;
 struct PosAndAcc {
 	int position;
 	Accidental accidental = Accidental::kNone;
+	bool tie_to_prev = false;
+	bool tie_to_next = false;
 };
 
 struct Chord {
@@ -55,6 +57,42 @@ glyph_for_accidental (Accidental a)
 		return SMuFL::Glyph::kNumEntries;
 	}
 }
+
+struct Rest {
+	SMuFL::Glyph g;
+	Beats b;
+	int line = 2;
+};
+
+Rest kRests[] = {
+	{ SMuFL::Glyph::kRestWhole, Beats (4, 0), 3 },
+	{ SMuFL::Glyph::kRestHalf, Beats (2, 0) },
+	{ SMuFL::Glyph::kRestQuarter, Beats (1, 0) },
+	{ SMuFL::Glyph::kRest8th, Beats (0, Temporal::ticks_per_beat / 2) },
+	{ SMuFL::Glyph::kRest16th, Beats (0, Temporal::ticks_per_beat / 4) },
+	{ SMuFL::Glyph::kRest32nd, Beats (0, Temporal::ticks_per_beat / 8) },
+	{ SMuFL::Glyph::kRest64th, Beats (0, Temporal::ticks_per_beat / 16) },
+	{ SMuFL::Glyph::kRest128th, Beats (0, Temporal::ticks_per_beat / 32) },
+};
+
+struct NoteShape {
+	SMuFL::Glyph notehead_glyph;
+	Beats b;
+	bool uses_stem = true;
+	int flag_count = 0;
+};
+
+NoteShape kNotes[] = {
+	{ SMuFL::Glyph::kNoteheadDoubleWhole, Beats (8, 0), false },
+	{ SMuFL::Glyph::kNoteheadWhole, Beats (4, 0), false },
+	{ SMuFL::Glyph::kNoteheadHalf, Beats (2, 0) },
+	{ SMuFL::Glyph::kNoteheadBlack, Beats (1, 0) },
+	{ SMuFL::Glyph::kNoteheadBlack, Beats (0, Temporal::ticks_per_beat / 2), true, 1 },
+	{ SMuFL::Glyph::kNoteheadBlack, Beats (0, Temporal::ticks_per_beat / 4), true, 2 },
+	{ SMuFL::Glyph::kNoteheadBlack, Beats (0, Temporal::ticks_per_beat / 8), true, 3 },
+	{ SMuFL::Glyph::kNoteheadBlack, Beats (0, Temporal::ticks_per_beat / 16), true, 4 },
+	{ SMuFL::Glyph::kNoteheadBlack, Beats (0, Temporal::ticks_per_beat / 32), true, 5 },
+};
 
 } // namespace
 
@@ -120,6 +158,8 @@ MidiScoreBar::render (const ArdourCanvas::Rect &area, Cairo::RefPtr<Cairo::Conte
 		//  - make list of (timestamp-rounded-to-small-number, notes-on-at-time)
 		//  - then iterate over that list, emitting notes and rests as needed
 
+		// TODO: separate out layout from rendering
+
 		// start out with no accidentals
 		std::map<int, Accidental> accidentals;
 		std::vector<Chord> chords;
@@ -132,7 +172,7 @@ MidiScoreBar::render (const ArdourCanvas::Rect &area, Cairo::RefPtr<Cairo::Conte
 			if (a != note_to_render.accidental) {
 				a = note_to_render.accidental;
 				if (a == Accidental::kNone) {
-					// Going from soemthing to none means we're going back to the key signature.
+					// Going from something to none means we're going back to the key signature.
 					// That means we do need an accidental.
 					if (ks->flats_or_sharps_count() < 0) {
 						pa.accidental = Accidental::kFlat;
@@ -145,15 +185,56 @@ MidiScoreBar::render (const ArdourCanvas::Rect &area, Cairo::RefPtr<Cairo::Conte
 			}
 
 			Chord c;
-			c.position = np.note->time() + np.offset;
-			c.duration = np.note->length();
-			c.notes.push_back (pa);
-			chords.push_back (c);
+			// TODO: something better than rounding to 32nd
+			c.position = (np.note->time() + np.offset).round_to_subdivision (8, Temporal::RoundNearest);
+			c.duration = np.note->length().round_to_subdivision (8, Temporal::RoundNearest);
+
+			if (!chords.empty() && chords.back().position == c.position
+			    && chords.back().duration == c.duration) {
+				chords.back().notes.push_back (pa);
+			} else {
+				c.notes.push_back (pa);
+				chords.push_back (c);
+			}
 		}
 
+		for (auto &c : chords) {
+			sort (c.notes.begin(), c.notes.end(),
+			      [] (const PosAndAcc &a, const PosAndAcc &b) { return a.position < b.position; });
+
+			// TODO: deal with clashing positions. Change one of the clashing notes to the alternate option
+			// for that note.
+		}
+
+		Beats last_end = _beat;
 		for (const auto &c : chords) {
-			SMuFL::Glyph note_head_glyph = SMuFL::Glyph::kNoteheadBlack;
-			std::string s = SMuFL::GlyphAsUTF8 (note_head_glyph);
+			// Fill in gap with rests
+			if (c.position > last_end) {
+				Beats rest_length = c.position - last_end;
+				std::cerr << rest_length << " worth of rests" << std::endl;
+				// TODO: take time signature into account, and round to whole beats first etc.
+				for (const auto &r : kRests) {
+					std::cerr << "Checking for " << r.b << " as rest." << std::endl;
+					while (rest_length >= r.b) {
+						rest_length -= r.b;
+
+						std::string s = SMuFL::GlyphAsUTF8 (r.g);
+						Cairo::TextExtents rest_extents;
+						cr->get_text_extents (s, rest_extents);
+						cr->move_to (x, self.y1 - r.line * ld);
+						cr->show_text (s);
+						x += rest_extents.width + 2;
+					}
+				}
+			}
+			last_end = c.position + c.duration;
+
+			// TODO: preprocess into correct note segments
+			NoteShape *note_shape = kNotes;
+			while (note_shape->b > c.duration)
+				note_shape++;
+
+			std::string s = SMuFL::GlyphAsUTF8 (note_shape->notehead_glyph);
 			Cairo::TextExtents note_extents;
 			cr->get_text_extents (s, note_extents);
 
@@ -180,6 +261,26 @@ MidiScoreBar::render (const ArdourCanvas::Rect &area, Cairo::RefPtr<Cairo::Conte
 			int top_note_position = -9999;
 			int bottom_note_position = 9999;
 
+			double stem_width = 0.1 * ld;
+			double right_note_offset = note_extents.width - stem_width;
+
+			bool prev_left = true;
+			bool has_notes_on_right = false;
+			int last_position = -9999;
+			for (const auto &n : c.notes) {
+				if (n.position == last_position + 1) {
+					has_notes_on_right = true;
+					break;
+				}
+				last_position = n.position;
+			}
+
+			last_position = -9999;
+
+			double leger_note_length = note_extents.width;
+			if (has_notes_on_right)
+				leger_note_length += right_note_offset;
+
 			for (const auto &n : c.notes) {
 				top_note_position = std::max (top_note_position, n.position);
 				bottom_note_position = std::min (bottom_note_position, n.position);
@@ -190,44 +291,82 @@ MidiScoreBar::render (const ArdourCanvas::Rect &area, Cairo::RefPtr<Cairo::Conte
 				for (int i = n.position / 2; i < bottom_line; ++i) {
 					double y = self.y1 - i * _view.line_distance();
 					cr->move_to (x - extension, y);
-					cr->line_to (x + note_extents.width + extension, y);
+					cr->line_to (x + leger_note_length + extension, y);
 					cr->stroke();
 				}
 				for (int i = top_line + 1; i <= n.position / 2; ++i) {
 					double y = self.y1 - i * _view.line_distance();
 					cr->move_to (x - extension, y);
-					cr->line_to (x + note_extents.width + extension, y);
+					cr->line_to (x + leger_note_length + extension, y);
 					cr->stroke();
 				}
 				bottom_line = std::min (bottom_line, n.position / 2);
 				top_line = std::max (top_line, n.position / 2);
 
+				double note_x = x;
+				if (n.position == last_position + 1 && prev_left) {
+					note_x = x + right_note_offset;
+					prev_left = false;
+				} else {
+					prev_left = true;
+				}
 				// Note head
 				double y = self.y1 - n.position * ld / 2;
-				cr->move_to (x, y);
+				cr->move_to (note_x, y);
 				cr->show_text (s);
+
+				last_position = n.position;
 			}
 
 			// Chord stem
-			double stem_width = 0.1 * ld;
-			cr->set_line_width (stem_width);
-			bool stem_down = top_note_position > 4;
-			double top_y = self.y1 - top_note_position * ld / 2;
-			double bottom_y = self.y1 - bottom_note_position * ld / 2;
-			if (stem_down) {
-				// down
-				double stemx = 0.0 + 0.5 * stem_width;
-				cr->move_to (x + stemx, top_y + 0.168 * ld);
-				cr->line_to (x + stemx, bottom_y + 4 * ld);
-			} else {
-				// up
-				double stemx = 1.3 * ld - 0.5 * stem_width;
-				cr->move_to (x + stemx, bottom_y - 0.16 * ld);
-				cr->line_to (x + stemx, top_y - 4 * ld);
-			}
-			cr->stroke();
+			if (note_shape->uses_stem) {
+				int stem_length = 4;
+				if (note_shape->flag_count > 2)
+					stem_length += note_shape->flag_count - 2;
 
-			x += note_extents.width + 10;
+				cr->set_line_width (stem_width);
+				bool stem_down = top_note_position > 4;
+				double top_y = self.y1 - top_note_position * ld / 2;
+				double bottom_y = self.y1 - bottom_note_position * ld / 2;
+				if (stem_down) {
+					// down
+					double stemx = 0.0 + 0.5 * stem_width;
+					if (has_notes_on_right)
+						stemx += right_note_offset;
+					cr->move_to (x + stemx, top_y + 0.168 * ld);
+					cr->line_to (x + stemx, bottom_y + stem_length * ld);
+
+					if (note_shape->flag_count) {
+						SMuFL::Glyph flag_g = note_shape->flag_count == 1
+						                          ? SMuFL::Glyph::kFlag8thDown
+						                          : SMuFL::Glyph::kFlag16thDown;
+						cr->move_to (x + stemx - 0.5 * stem_width,
+						             bottom_y + stem_length * ld);
+						std::string flag_s = SMuFL::GlyphAsUTF8 (flag_g);
+						cr->show_text (flag_s);
+					}
+				} else {
+					// up
+					double stemx = 1.3 * ld - 0.5 * stem_width;
+					cr->move_to (x + stemx, bottom_y - 0.16 * ld);
+					cr->line_to (x + stemx, top_y - stem_length * ld);
+
+					if (note_shape->flag_count) {
+						SMuFL::Glyph flag_g = note_shape->flag_count == 1
+						                          ? SMuFL::Glyph::kFlag8thUp
+						                          : SMuFL::Glyph::kFlag16thUp;
+						cr->move_to (x + stemx - 0.5 * stem_width, top_y - stem_length * ld);
+						std::string flag_s = SMuFL::GlyphAsUTF8 (flag_g);
+						cr->show_text (flag_s);
+					}
+				}
+				cr->stroke();
+			}
+
+			// TODO: evenly distribute
+			x += note_extents.width + 2 + x_offset;
+			if (has_notes_on_right)
+				x += right_note_offset;
 		}
 	}
 }
